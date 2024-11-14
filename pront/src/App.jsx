a6545491e2a41c4cf15e8c1177c3b01f27ea6473
qortn4925 <qortn4925@naver.com>
2024-11-14 10:37:33 +0900

    글 작성 form ,입력 받은값을  spring으로

--- pront/src/App.jsx
+++ pront/src/App.jsx
@@ -5,7 +5,10 @@ import {
   RouterProvider,
   useNavigate,
 } from "react-router-dom";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import { Box, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import { Field } from "./components/ui/field.jsx";
+import { Button } from "./components/ui/button.jsx";
+import axios from "axios";
 
 function Navbar() {
   const navigate = useNavigate();
@@ -39,9 +42,38 @@ function BoardList() {
 }
 
 function BoardAdd() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  const handleSaveClick = () => {
+    axios.post("/api/board/add", {
+      //   객체로 보낼 속성명과 ,  넘겨줄 변수명이 같으면 생략 가능
+      title,
+      content: content,
+      writer: writer,
+    });
+  };
   return (
     <Box>
       <h3> 안녕</h3>
+      <Stack gap={5}>
+        <Field>
+          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </Field>
+        <Field label={"본문"}>
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Field>
+        <Field label={"작성자"}>
+          <Input value={writer} onChange={(e) => setWriter(e.target.value)} />
+        </Field>
+        <Box>
+          <Button onClick={handleSaveClick}>저장</Button>
+        </Box>
+      </Stack>
     </Box>
   );
 }
@@ -56,7 +88,7 @@ const router = createBrowserRouter([
         element: <BoardList />,
       },
       {
-        index: "add",
+        path: "add",
         element: <BoardAdd />,
       },
     ],
