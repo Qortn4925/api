4092bdf6c50a9164b955d3eead80c58bd48c38a2
qortn4925 <qortn4925@naver.com>
2024-11-15 14:32:39 +0900

    조회된 결과가 없을때 테이블 대신 메세지 출력

--- pront/src/page/board/BoardList.jsx
+++ pront/src/page/board/BoardList.jsx
@@ -83,27 +83,34 @@ export function BoardList() {
   return (
     <Box>
       <h3> 게시글 목록</h3>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>번호</Table.ColumnHeader>
-            <Table.ColumnHeader>제목</Table.ColumnHeader>
-            <Table.ColumnHeader>작성자</Table.ColumnHeader>
-            <Table.ColumnHeader>작성일시</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {boardList.map((board) => (
-            // 파라미터가 필요해서 , 함수를 넣어서  파라미터 넣어줌
-            <Table.Row key={board.id} onClick={() => handleRowClick(board.id)}>
-              <Table.Cell> {board.id}</Table.Cell>
-              <Table.Cell> {board.title}</Table.Cell>
-              <Table.Cell> {board.writer}</Table.Cell>
-              <Table.Cell> {board.inserted}</Table.Cell>
+      {boardList.length > 0 ? (
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>번호</Table.ColumnHeader>
+              <Table.ColumnHeader>제목</Table.ColumnHeader>
+              <Table.ColumnHeader>작성자</Table.ColumnHeader>
+              <Table.ColumnHeader>작성일시</Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {boardList.map((board) => (
+              // 파라미터가 필요해서 , 함수를 넣어서  파라미터 넣어줌
+              <Table.Row
+                key={board.id}
+                onClick={() => handleRowClick(board.id)}
+              >
+                <Table.Cell> {board.id}</Table.Cell>
+                <Table.Cell> {board.title}</Table.Cell>
+                <Table.Cell> {board.writer}</Table.Cell>
+                <Table.Cell> {board.inserted}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      ) : (
+        <p> 조회된 결과가 없습니다.</p>
+      )}
       <HStack>
         <NativeSelectRoot
           onChange={(e) => setSearch({ ...search, type: e.target.value })}
