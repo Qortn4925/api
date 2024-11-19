bd6e42c043b256f4cbaa9d66152689b8617bb8d8
qortn4925 <qortn4925@naver.com>
2024-11-19 16:33:55 +0900

    navbar 에서  분해할당으로 코드

--- pront/src/components/root/Navbar.jsx
+++ pront/src/components/root/Navbar.jsx
@@ -6,28 +6,26 @@ import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
 export function Navbar() {
   const navigate = useNavigate();
   //step2 : context 사용하기!
-  const authentication = useContext(AuthenticationContext);
+  const { id, isAdmin, isAuthenticated, logout } = useContext(
+    AuthenticationContext,
+  );
 
   return (
     <Flex gap={3}>
       <Box onClick={() => navigate("/")}> home</Box>
-      {authentication.isAuthenticated && (
-        <Box onClick={() => navigate("/add")}> 글 작성</Box>
-      )}
-      {authentication.isAuthenticated || (
+      {isAuthenticated && <Box onClick={() => navigate("/add")}> 글 작성</Box>}
+      {isAuthenticated || (
         <Box onClick={() => navigate("/member/signup")}> 회원 가입</Box>
       )}
-      {authentication.isAdmin && (
-        <Box onClick={() => navigate("/member/list")}>회원 목록</Box>
-      )}
-      {authentication.isAuthenticated || (
+      {isAdmin && <Box onClick={() => navigate("/member/list")}>회원 목록</Box>}
+      {isAuthenticated || (
         <Box onClick={() => navigate("/member/login")}>로그인</Box>
       )}
 
-      {authentication.isAuthenticated && (
+      {isAuthenticated && (
         <Box
           onClick={() => {
-            authentication.logout(); // 토큰 없어짐
+            logout(); // 토큰 없어짐
             // login 가서는 글 작성 안보여야 하고 , 로그아웃이 아닌 로그인이 보여야 하는거 아니가 ?
             navigate("/member/login");
           }}
@@ -38,7 +36,7 @@ export function Navbar() {
       )}
       <Box variant={"outline"} bg={"black.300"}>
         {" "}
-        {authentication.id}
+        {id}
       </Box>
     </Flex>
   );
