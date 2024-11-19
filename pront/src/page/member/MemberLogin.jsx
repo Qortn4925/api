b06db441363b0fd141ee52c56b22752915c6cca1
qortn4925 <qortn4925@naver.com>
2024-11-19 11:36:37 +0900

    로그인

--- pront/src/page/member/MemberLogin.jsx
+++ pront/src/page/member/MemberLogin.jsx
@@ -2,13 +2,37 @@ import { Box, Button, Input, Stack } from "@chakra-ui/react";
 import { Field } from "../../components/ui/field.jsx";
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toaster } from "../../components/ui/toaster.jsx";
 
 export function MemberLogin() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   function handleLoginClick() {
-    axios.post("/api/member/login", { id, password }).then().catch().finally();
+    axios
+      .post("/api/member/login", { id, password })
+      .then((res) => {
+        res.data;
+      })
+      .then((data) => {
+        //토스트 띄우고
+        toaster.create({
+          type: data.message.type,
+          description: data.message.text,
+        });
+        // 이동
+        // local Storage에 token 저장
+      })
+      .catch((e) => {
+        // 실패 토스트
+        toaster.create({
+          type: e.response.data.message.type,
+          description: e.response.data.message.text,
+        });
+      })
+      .finally();
   }
 
   return (
