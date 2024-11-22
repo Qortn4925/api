fde3077caf2ee3fc4f4f90408040ecce4a61e0c2
qortn4925 <qortn4925@naver.com>
2024-11-22 14:21:42 +0900

    navbar 회원 메뉴는 오른쪽 배치

--- pront/src/components/root/Navbar.jsx
+++ pront/src/components/root/Navbar.jsx
@@ -3,6 +3,24 @@ import { Box, Flex } from "@chakra-ui/react";
 import { useContext } from "react";
 import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
 
+function NavbarItem({ children, ...rest }) {
+  return (
+    <Box
+      css={{
+        paddingX: "15px",
+        paddingY: "10px",
+      }}
+      _hover={{
+        bgColor: "cyan.500",
+        cursor: "pointer",
+      }}
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+}
+
 export function Navbar() {
   const navigate = useNavigate();
   //step2 : context 사용하기!
@@ -12,27 +30,39 @@ export function Navbar() {
   //  && or 는 true 만나면 바로 true 리턴 ,      ||and 는  falsy 를 만나자 마자 그걸 리턴하고 , 둘다 트루면 마지막
   return (
     <Flex gap={3}>
-      <Box onClick={() => navigate("/")}> home</Box>
-      {isAuthenticated && <Box onClick={() => navigate("/add")}> 글 작성</Box>}
-      {isAuthenticated || (
-        <Box onClick={() => navigate("/member/signup")}> 회원 가입</Box>
+      <NavbarItem onClick={() => navigate("/")}> home</NavbarItem>
+      {isAuthenticated && (
+        <NavbarItem onClick={() => navigate("/add")}> 글 작성</NavbarItem>
       )}
-      {isAdmin && <Box onClick={() => navigate("/member/list")}>회원 목록</Box>}
+      <Box mx={"auto"}></Box>
       {isAuthenticated || (
-        <Box onClick={() => navigate("/member/login")}>로그인</Box>
+        <NavbarItem onClick={() => navigate("/member/signup")}>
+          {" "}
+          회원 가입
+        </NavbarItem>
+      )}
+      {isAdmin && (
+        <NavbarItem onClick={() => navigate("/member/list")}>
+          회원 목록
+        </NavbarItem>
+      )}
+      {isAuthenticated || (
+        <NavbarItem onClick={() => navigate("/member/login")}>
+          로그인
+        </NavbarItem>
       )}
 
       {isAuthenticated && (
-        <Box
+        <NavbarItem
           onClick={() => {
             logout();
             navigate("/member/login");
           }}
         >
           로그아웃
-        </Box>
+        </NavbarItem>
       )}
-      <Box onClick={() => navigate(`/member/${id}`)}>{id}</Box>
+      <NavbarItem onClick={() => navigate(`/member/${id}`)}>{id}</NavbarItem>
     </Flex>
   );
 }
